48e9c90ae076aaf2f49a209d4a225f921e2e7c48
ParisaMousavi <parisamoosavinezhad@yahoo.com>
2022-09-05 22:38:16 +0200

    update

---
+++ input.tf
@@ -0,0 +1,20 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "sku" {
+  type    = string
+}
+
+variable "admin_enabled" {
+  type    = bool
+  default = false
+}
+
+variable "name" {
+  type = string
+}
---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "azurerm_container_registry" "this" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  sku                 = var.sku
+  admin_enabled       = var.admin_enabled
+  retention_policy {
+    days    = 7
+    enabled = true
+  }
+  tags = {
+    created-by = "terraform"
+  }
+}
