23c1259ddb6365670c9822291004d674b664e17b
ParisaMousavi <parisamoosavinezhad@yahoo.com>
2023-02-28 23:22:17 +0100

    update

--- main.tf
+++ main.tf
@@ -59,12 +59,3 @@ resource "azurerm_private_endpoint" "this" {
     },
   )
 }
-
-resource "azurerm_private_dns_zone_virtual_network_link" "this" {
-  count                 = var.private_endpoint_config.private_dns_zone_name != null && var.private_endpoint_config.virtual_network_id != null ? 1 : 0
-  name                  = "${var.name}-vnet2dns"
-  resource_group_name   = var.resource_group_name
-  private_dns_zone_name = var.private_endpoint_config.private_dns_zone_name
-  virtual_network_id    = var.private_endpoint_config.virtual_network_id
-}
-
